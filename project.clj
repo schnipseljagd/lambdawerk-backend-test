31a1979c2de2f4950b42e334393e3863a73b7c21
Joscha <joscha@schnipseljagd.org>
2018-01-22 21:35:28 +0100

    first spike

--- project.clj
+++ project.clj
@@ -2,10 +2,12 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/java.jdbc "0.7.5"]]
+                 [org.clojure/java.jdbc "0.7.5"]
+                 [org.postgresql/postgresql "42.2.0"]
+                 [hikari-cp "2.0.1"]]
 
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [org.clojure/test.check "0.9.0"]]
--- src/lambdawerk_backend_test/core.clj
+++ src/lambdawerk_backend_test/core.clj
@@ -1,12 +1,14 @@
 (ns lambdawerk-backend-test.core
   (:require [clojure.java.io :as io]
-            [clojure.instant :refer [read-instant-date]]
+            [clojure.instant :refer [read-instant-timestamp]]
             [clojure.spec.alpha :as s]
             [clojure.string :refer [blank?]]
             [clojure.spec.test.alpha :as stest]
             [lambdawerk-backend-test.xml-reader :as xml]
-            [clojure.java.jdbc :as j])
-  (:import (java.io Reader)))
+            [clojure.java.jdbc :as j]
+            [hikari-cp.core :refer [make-datasource close-datasource]])
+  (:import (java.io Reader)
+           (java.util.concurrent Executors ExecutorService)))
 
 (s/def ::firstname (s/and string? (complement blank?)))
 (s/def ::lastname (s/and string? (complement blank?)))
@@ -32,7 +34,7 @@
 
 (defn parse-date-of-birth [s]
   (try
-    (read-instant-date s)
+    (read-instant-timestamp s)
     (catch RuntimeException e
       (swap! broken-dates update s (fnil inc 0))
       nil)))
@@ -44,29 +46,85 @@
 
 (defn ->person [{:keys [date-of-birth] :as member}]
   (-> member
-    (update :date-of-birth parse-date-of-birth)
-    (update :phone parse-int)))
+      (update :date-of-birth parse-date-of-birth)
+      (update :phone parse-int)))
 
-(defn do-something [member]
+(defn validate-person [member]
   (s/assert ::person member)
   member)
 
-(defn read-member-updates [^Reader reader]
+(defn xml->persons [^Reader reader]
   (-> reader
       (xml/parse)
       (xml/get-members)
-      (->> (pmap (comp do-something ->person xml/member->map)))))
+      (->> (pmap (comp validate-person ->person xml/member->map)))))
 
-(defn update-persons-table [persons]
-  (doseq [persons-chunk (partition 10 persons)]
-    (prn persons-chunk)))
+(def insert-counter (atom 0))
+
+(defn run-in-parallel [data function number-of-executors]
+  (let [service (Executors/newFixedThreadPool number-of-executors)
+        submit (fn [function data] (.submit ^ExecutorService service
+                                            ^Callable (fn [] (function data))))]
+    (->> data
+         (map (partial submit function))
+         (doall)
+         (map deref)
+         (doall))))
+
+(comment
+  (run-in-parallel [1 2 3 4 5 6 7 8 9 10]
+                   (fn [in] (prn in))
+                   4))
+
+(def datasource-options {:auto-commit       false
+                         :minimum-idle      1
+                         :maximum-pool-size 10
+                         :pool-name         "db-pool"
+                         :adapter           "postgresql"
+                         :username          "postgres"
+                         :password          "password"
+                         :database-name     "postgres"
+                         :server-name       "localhost"})
+
+(defn insert-or-update-persons-table [datasource persons]
+  (j/with-db-transaction
+    [t-con {:datasource datasource}]
+    (doseq [{:keys [firstname lastname phone date-of-birth]} persons]
+      (j/execute! t-con
+                  ["insert into person (fname,lname,dob) values (?, ?, ?) on conflict (fname,lname,dob) do update set phone = ? where person.phone != ?"
+                   firstname
+                   lastname
+                   date-of-birth
+                   (str phone)
+                   (str phone)])))
+
+  (prn "insert: " (swap! insert-counter inc)))
+
+(defn update-persons-table [persons
+                            {:keys [transaction-chunk-size number-of-executors]}
+                            datasource-options]
+  (let [datasource (make-datasource datasource-options)]
+    (-> (partition transaction-chunk-size persons)
+        (run-in-parallel (partial insert-or-update-persons-table datasource)
+                         number-of-executors))
+    (close-datasource datasource)))
 
 (s/check-asserts true)
 
 (comment
+  (j/query pg-db "select * from person limit 10")
+  (j/execute! pg-db "ALTER TABLE person ADD UNIQUE (fname,lname,dob)")
   ;(with-open [reader (io/reader "update-file.xml")]
   (time (-> (io/reader "update-file.xml")
-            (read-member-updates)
-            (->> (take 100))
-            (update-persons-table))))
+            (xml->persons)
+            (update-persons-table {:transaction-chunk-size 10000
+                                   :number-of-executors    4}
+                                  {:minimum-idle      1
+                                   :maximum-pool-size 10
+                                   :pool-name         "db-pool"
+                                   :adapter           "postgresql"
+                                   :username          "postgres"
+                                   :password          "password"
+                                   :database-name     "postgres"
+                                   :server-name       "localhost"}))))
 

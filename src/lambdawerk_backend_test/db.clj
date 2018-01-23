aeac739711e4244786503651d6fe05a73d18bc93
Joscha <joscha@schnipseljagd.org>
2018-01-23 20:31:00 +0100

    fix insert counting

--- src/lambdawerk_backend_test/db.clj
+++ src/lambdawerk_backend_test/db.clj
@@ -3,7 +3,7 @@
             [clojure.java.jdbc :as j]
             [util.async :as async]))
 
-(def insert-counter (atom 0))
+(def insert-counter (atom nil))
 
 (def insert-or-update-persons-table-query
   "insert into person (fname,lname,dob) values (?, ?, ?) on conflict (fname,lname,dob) do update set phone = ? where person.phone != ?")
@@ -28,6 +28,7 @@
 (defn update-persons-table [persons
                             {:keys [transaction-chunk-size number-of-executors]}
                             datasource-options]
+  (reset! insert-counter 0)
   (let [datasource (make-datasource datasource-options)]
     (-> (partition transaction-chunk-size persons)
         (async/run-in-parallel (partial insert-or-update-persons-table datasource)
